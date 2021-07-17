3943e5a25a73718aa337d750bd30ae7ba5d60d80
Rakibul Islam <rakibulislam4001@gmail.com>
2021-07-18 00:57:26 +0600

    c-sharp

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Add
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int num1, num2, add;
+			Console.Write("Enter 1st number: ");
+			num1=Convert.ToInt16(Console.ReadLine());
+			Console.Write("Enter 2nd number: ");
+			num2=Convert.ToInt16(Console.ReadLine());
+			add=num1+num2;
+			Console.Write("Summation is: " + add);
+			Console.ReadKey(true);
+		}
+	}
+}
